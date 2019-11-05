b1d81e163e92f612d1a23b85d530c8bca66ba76d
Ahmed Hamdy <a27med@gmail.com>
2019-11-05 02:06:43 -0800

    Cleaning up issues with network evaluation

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import os
+import sys
 import mir_eval
 import torch
 import torch.nn as nn
@@ -116,7 +117,7 @@ def train(model):
 				target[i] = torch.FloatTensor(target[i])
 				target[i] = target[i][:, :, :].permute(0, 3, 1, 2)
 			
-			concat_target = torch.cat((target[0], target[1], target[2]), 1)
+			concat_target = torch.cat((target[0], target[1], target[2], target[3]), 1)
 			
 			loss = criterion(model_out, concat_target)
 			loss_list.append(loss.item())
@@ -147,20 +148,29 @@ def eval(model):
 	sar_list = []
 
 	for (data, target) in (io_processing.dev_set):
+		data = torch.FloatTensor(data)
 		model_out = model(data.permute(0, 3, 1, 2))
+		model_out = model_out.permute(0, 2, 3, 1)
+
+		model_estimate = []
 
 		for i in range(len(target)):
-			target[i] = torch.FloatTensor(target[i])
-			target[i] = target[i][:, :, :].permute(0, 3, 1, 2)
-		
-		concat_target = torch.cat((target[0], target[1], target[2]), 1)
+			model_estimate[i] = io_processing.PostProcessData(model_out[:,:,:,i*(2):i*(2)+2])	
 
-		[sdr, isr, sir, sar, _] = mir_eval.separation.bss_eval_images(concat_target.numpy(), model_out.numpy())
+			target[i] = torch.FloatTensor(target[i])
+			target[i] = io_processing.PostProcessData(target[i])
+		
+		concat_estimate = torch.cat((model_estimate[0], model_estimate[1], model_estimate[2], model_estimate[3]), 1)
+		concat_target = torch.cat((target[0], target[1], target[2], target[3]), 1)
+
+		[sdr, isr, sir, sar, _] = mir_eval.separation.bss_eval_images(concat_target.detach().numpy(), concat_estimate.detach().numpy())
 		
 		sdr_list.append(sdr)
-        isr_list.append(isr)
-        sir_list.append(sir)
-        sar_list.append(sar)
+		isr_list.append(isr)
+		sir_list.append(sir)
+		sar_list.append(sar)
+
+	return (sdr_list, isr_list, sir_list, sar_list)
 
 def init_log():
 	if not os.path.isdir(config.log_folder):
@@ -172,11 +182,19 @@ def init_log():
 	if not os.path.isdir(config.log_complete_model_folder):
 		os.mkdir(config.log_complete_model_folder)
 
-init_log()
+if __name__ == '__main__':
+	init_log()
 
-model = AutoEncoderModel()
-optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)
-criterion = nn.MSELoss()
+	model = AutoEncoderModel()
+	optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)
+	criterion = nn.MSELoss()
 
-train(model)
-eval(model)
+	if sys.argv[1] == '--train' or sys.argv[1] == '--t':
+		print("Training the Autoencoder Model...")
+		train(model)
+	elif sys.argv[1] == '--eval' or sys.argv[1] == '--e':
+		print("Evaluating the Autoenconder Model...")
+		eval(model)
+    else:
+        print("%s --train to train the autoencoder model"%sys.argv[0])
+        print("%s --eval to evaluate the autoencoder model"%sys.argv[0])
